fdf6b87044c8c27c7b34b4b40b53d6a70465884f
Apin Ridwan <apinrdw@gmail.com>
2015-01-24 04:46:40 +0000

    added functionality to call action on another controller

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  def index
+    the_params = {post: {title: 'Post Title', body: 'Post Body 2'}}
+    controller = PostsController.new
+    controller.params = the_params
+    controller.request = ActionDispatch::Request.new(request.env)
+    controller.response = ActionDispatch::Response.new
+    controller.create
+  end
+end
